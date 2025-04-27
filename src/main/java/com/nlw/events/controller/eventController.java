4c96be8b30c42fef5cb14f7eecddf50a22b72469
Luiz <kubaszewski001@hotmail.com>
2025-04-27 18:48:39 -0300

    Criado o método para retornar por um pretty Name especifico e adicionado uma validação para se caso retornar null, retornar erro 404 not found.

--- src/main/java/com/nlw/events/controller/eventController.java
+++ src/main/java/com/nlw/events/controller/eventController.java
@@ -3,6 +3,7 @@ package com.nlw.events.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nlw.events.model.Event;
@@ -10,6 +11,7 @@ import com.nlw.events.service.EventService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -29,6 +31,16 @@ public class eventController {
     public List<Event> getAllEvents() {
         return service.getAllEvents();
     }
+
+    @GetMapping("/events/{prettyName}")
+    public ResponseEntity<Event> getEventByPrettyName(@PathVariable String prettyName) {
+        Event event =  service.getByPrettyName(prettyName);
+        if(event != null) {
+            return ResponseEntity.ok().body(event);
+        } 
+        return ResponseEntity.notFound().build();
+    }
+    
     
     
 }
